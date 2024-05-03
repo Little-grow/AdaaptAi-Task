3b6571a755d3230f9a75b64eadd4395bce9d9a93
Shahd El-Badrawy <shahdbadrawy@gmail.com>
2024-05-03 11:56:26 +0300

    fix bug

--- Adappt-Task-Api/DTO/TokenRequestDto.cs
+++ Adappt-Task-Api/DTO/TokenRequestDto.cs
@@ -4,7 +4,7 @@ namespace WebApplication1.DTO
 {
     public class TokenRequestDto
     {
-        public string Email { get; internal set; }
-        public string Password { get; internal set; }
+        public string Email { get; set; } 
+        public string Password { get; set; }
     }
 }
